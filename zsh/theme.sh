f4a1225161c8e07296dfb35c3e66197447d70590
Henry Seong Wook Ha <seong.ha@lendinghome.com>
2019-06-28 15:26:48 -0400

    add .gitignore support to fzf through fd

--- zsh/theme.sh
+++ zsh/theme.sh
@@ -76,3 +76,6 @@ alias cls="clear"
 alias search="sudo grep --color -rnw ./ -e "
 
 eval $(thefuck --alias)
+
+export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
