f7c627b4c2f1921b48090790ff27bcfa980a88ab
Henry Ha <henry.ha@lendinghome.com>
2020-01-15 13:12:30 -0500

    add some more stuff to theme.sh

--- zsh/theme.sh
+++ zsh/theme.sh
@@ -83,3 +83,16 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 alias yarn-ops="cd ~/code/lendinghome-monolith/ops && yarn build:watch || osascript -e 'display notification \"Gosh Yarn-it, I crashed\" with title \"💩 💩 \"'"
 alias rspec="bundle exec rspec"
 alias cucumber="bundle exec cucumber"
+
+setopt histignorespace
+
+export YVM_DIR=/usr/local/opt/yvm
+[ -r $YVM_DIR/yvm.sh ] && . $YVM_DIR/yvm.sh
+# Setting for the new UTF-8 terminal support in Lion
+LC_CTYPE=en_US.UTF-8
+LC_ALL=en_US.UTF-8
+
+export RUBY_CONFIGURE_OPTS="--disable-dtrace --with-openssl-dir=`brew --prefix openssl` --with-readline-dir=`brew --prefix readline` --with-libyaml-dir=`brew --prefix libyaml` --with-zlib-dir=`brew --prefix zlib`"
+
+export BUILDKITE_TOKEN=15c404b9c0a718b574a61f943c0314571c53d34e
+export BUILDKITE_API_TOKEN=$BUILDKITE_TOKEN
