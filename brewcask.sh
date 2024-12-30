868c8457795684ed9cf059e4e988a3e117cf0e1e
하성욱[쇼핑서비스FE] <seongwook.ha@navercorp.com>
2024-12-30 17:45:36 +0900

    add arc

--- brewcask.sh
+++ brewcask.sh
@@ -1,1 +1,1 @@
-brew install google-chrome visual-studio-code alfred adobe-creative-cloud numi slack iina
+brew install google-chrome visual-studio-code alfred adobe-creative-cloud numi slack iina arc
