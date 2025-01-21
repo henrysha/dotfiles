4912020f520609888bffdf9dbc80e92c970199a9
USER <user@AL02491159.local>
2025-01-21 15:01:34 +0900

    add scripts to add generated key to the ssh config and use keychain

--- generateSSHKey.sh
+++ generateSSHKey.sh
@@ -1,2 +1,11 @@
 ssh-keygen -t ed25519 -C 'swh0901@gmail.com'
 pbcopy < ~/.ssh/id_ed25519.pub
+
+eval "$(ssh-agent -s)"
+echo "Host github.com
+  AddKeysToAgent yes
+  UseKeychain yes
+  IdentityFile ~/.ssh/id_ed25519" > ~/.ssh/config
+
+ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+
